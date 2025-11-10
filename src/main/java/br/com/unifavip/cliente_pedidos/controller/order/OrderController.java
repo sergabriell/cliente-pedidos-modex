4edf0db858e4bb26e9719512c4ca64323bcf27e3
Sergio Gabriel <sergio.gabriel@catskillet.com>
2025-11-10 16:47:11 -0300

    feat: Controllers

---
+++ src/main/java/br/com/unifavip/cliente_pedidos/controller/order/OrderController.java
@@ -0,0 +1,11 @@
+package br.com.unifavip.cliente_pedidos.controller.order;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/order")
+public class OrderController {
+}
---
+++ src/main/java/br/com/unifavip/cliente_pedidos/controller/product/ProductController.java
@@ -0,0 +1,11 @@
+package br.com.unifavip.cliente_pedidos.controller.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/product")
+public class ProductController {
+}
